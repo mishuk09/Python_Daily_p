2bbc1306a49dca6893568ac8b5dffd29a72eb013
mishuk09 <mishukinfo09@gmail.com>
2022-11-06 05:04:30 -0500

    Python_daily_p

--- Daily_practice.py
+++ Daily_practice.py
@@ -1,1 +1,34 @@
-print("Mahadi")
+
+# 1.Write a Program to create an empty valid class by name Students, with no properties.
+
+class student:
+    roll=""
+     
+    GPA=""
+
+mahadi=student()
+ 
+ 
+
+
+#2. Write a Program to create a class by name Students, and initialize attributes like name, age, and grade while creating an object.
+
+class student:
+    name=""
+    age=""
+    GPA=""
+
+mahadi=student()
+mahadi.name='Mahadi Hasan Mishuk'
+mahadi.age=23
+mahadi.GPA=5.00
+
+print(f"Name:{mahadi.name}\n{mahadi.age}\n{mahadi.GPA}")
+
+#3. Write a Program to create a class Student, having attributes name, age, grade, class, mobile and having methods set_attributes, get_attributes:
+
+
+
+
+
+ 
