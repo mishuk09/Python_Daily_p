b131142c42b1be04954850c41a0b34a5c2054e0c
mishuk09 <mishukinfo09@gmail.com>
2022-11-11 11:36:29 -0500

    python function

---
+++ function.py
@@ -0,0 +1,50 @@
+'''def myfunction():
+    print("Mahadi")
+    print("Hasan")
+    print("Mishuk")
+myfunction()'''
+
+'''hello()
+def hello():
+    print("mahadi")
+'''
+
+
+'''def welcome():
+    name=input()
+    print("Welcome "+name)
+welcome()'''
+
+
+
+'''def function(word):
+    print(word +  "!")
+
+function("mango")
+function("apple")
+function("orange")
+
+print(2* 3)'''
+
+
+'''
+def sum(x,y):
+    print(x+y)
+    print(x-y)
+sum(20,10)'''
+
+
+'''password = input()
+repeat = input()
+
+def validate(text1, text2):
+	
+	if text1==text2:
+		print("Correct")
+	else:
+		print("Wrong")
+		
+validate(password,repeat)'''
+
+
+
