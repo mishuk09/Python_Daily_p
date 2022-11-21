612969f4d090569c937dbf195bc36166693ed335
mishuk09 <mishukinfo09@gmail.com>
2022-11-21 11:49:45 -0500

    py regular practice

--- cm_m_func.py
+++ cm_m_func.py
@@ -1,8 +1,18 @@
 #cm to m
 
-def cm(x):
+'''def cm(x):
     m=x/100
     print(m)
-num=int(input("Enter the number"))
-cm(100)
 
+num=int(input("Enter the number="))
+cm(num)
+
+'''
+
+#m to cm
+
+def m(x):
+    cm=x*100
+    print(cm)
+num=int(input("Enter nnumber"))
+m(num)
