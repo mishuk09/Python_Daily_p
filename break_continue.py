9100dc3f9caefeafa772e0f3ccc2434eb69e45af
mishuk09 <mishukinfo09@gmail.com>
2022-11-21 11:45:05 -0500

    py  regular practice

---
+++ break_continue.py
@@ -0,0 +1,16 @@
+#Break
+i=1
+for i in range(10):
+
+    if i==6:
+        break
+    print(i)
+
+
+
+#continue
+
+for i in range(10):
+    if i==7:
+        continue
+    print(i)
---
+++ cm_m_func.py
@@ -0,0 +1,8 @@
+#cm to m
+
+def cm(x):
+    m=x/100
+    print(m)
+num=int(input("Enter the number"))
+cm(100)
+
